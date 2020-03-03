3350bad36b309ed049001e641303a5b7af248e2a
Siratee K <sirateek@gmail.com>
2020-03-03 15:10:42 +0700

    Add liff.shareTargetPicker()

--- js/liff-functions.js
+++ js/liff-functions.js
@@ -1,50 +1,79 @@
-function getProfile(){
-    liff.getProfile().then((profile) => {
-      console.log(JSON.stringify(profile))
-      window.alert(JSON.stringify(profile))
-    }).catch( (e) => {
-      console.log(e)
-      window.alert(e)
+function getProfile() {
+  liff
+    .getProfile()
+    .then(profile => {
+      console.log(JSON.stringify(profile));
+      window.alert(JSON.stringify(profile));
     })
-  }
-  function getAccessToken(){
-    window.alert(liff.getAccessToken())
-  }
-  function getContext(){
-    window.alert(JSON.stringify(liff.getContext()))
-  }
-
-  function sendMessage(){
-    liff.sendMessages([{"type": "text", "text": "Hello from LIFF2.0"}]).then( ()=> { window.alert("Message has been sent")}).catch((e) => {window.alert(e)})
-  }
-  
-  function login(){
-    liff.login()
-  }
-
-  function scanCode(){
-    liff.scanCode().then( (result) => {
-      window.alert(JSON.stringify(result))
-    }).catch( (e) => {
-      window.alert(e)
-    })
-  }
-
-  function openWindow(){
-    liff.openWindow({
-      url: "https://sirateek.me",
-      external: true
+    .catch(e => {
+      console.log(e);
+      window.alert(e);
     });
-  }
+}
+function getAccessToken() {
+  window.alert(liff.getAccessToken());
+}
+function getContext() {
+  window.alert(JSON.stringify(liff.getContext()));
+}
 
-  function closeWindow(){
-    liff.closeWindow()
+function sendMessage() {
+  liff
+    .sendMessages([{ type: "text", text: "Hello from LIFF2.0" }])
+    .then(() => {
+      window.alert("Message has been sent");
+    })
+    .catch(e => {
+      window.alert(e);
+    });
+}
+
+function login() {
+  liff.login();
+}
+
+function scanCode() {
+  liff
+    .scanCode()
+    .then(result => {
+      window.alert(JSON.stringify(result));
+    })
+    .catch(e => {
+      window.alert(e);
+    });
+}
+
+function openWindow() {
+  liff.openWindow({
+    url: "https://sirateek.me",
+    external: true
+  });
+}
+
+function closeWindow() {
+  liff.closeWindow();
+}
+
+function logout() {
+  if (liff.isLoggedIn()) {
+    liff.logout();
+    window.alert("Successfully to Logout");
+    location.reload();
   }
-  
-  function logout(){
-    if (liff.isLoggedIn()) {
-      liff.logout();
-      window.alert("Successfully to Logout")
-      location.reload();
-    }
-  }
+}
+
+function shareTargetPicker() {
+  liff
+    .shareTargetPicker([
+      {
+        type: "text",
+        text: "Hello from LIFF2.0!"
+      }
+    ])
+    .then(() => {
+      alert("Shared to the friend(s) you picked");
+    })
+    .catch(function(res) {
+      alert(res);
+    });
+}
